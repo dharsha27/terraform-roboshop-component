3930c167e6a0e5df849087d89d45aa21ffb29a44
dharsha27 <dharshavardhan27@gmail.com>
2026-08-08 19:55:06 +0530

    changes done

--- main.tf
+++ main.tf
@@ -41,39 +41,39 @@ resource "terraform_data" "main" {
 }
 
 
-# # 2. Control the runtime power state separately
-# resource "aws_ec2_instance_state" "catalogue" {
-#   instance_id = aws_instance.catalogue.id
-#   state       = "stopped" # Valid options: "running" or "stopped"
-#   depends_on  = [terraform_data.catalogue]
+# 2. Control the runtime power state separately
+resource "aws_ec2_instance_state" "main" {
+  instance_id = aws_instance.main.id
+  state       = "stopped" # Valid options: "running" or "stopped"
+  depends_on  = [terraform_data.main]
 
-#   # Optional: set to true if you need to force-kill a frozen instance
-#   # force = false 
-# }
+  # Optional: set to true if you need to force-kill a frozen instance
+  # force = false 
+}
 
-# resource "aws_ami_from_instance" "catalogue" {
-#   name               = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
-#   source_instance_id = aws_instance.catalogue.id
-#   depends_on         = [aws_ec2_instance_state.catalogue]
+resource "aws_ami_from_instance" "main" {
+  name               = "${local.common_name}-${var.app_version}-${aws_instance.main.id}"
+  source_instance_id = aws_instance.catalogue.id
+  depends_on         = [aws_ec2_instance_state.main]
 
-#   tags = merge(
-#     {
-#       Name = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
-#     },
-#     local.common_tags
-#   )
+  tags = merge(
+    {
+      Name = "${local.common_name}-${var.component}-${var.app_version}-${aws_instance.main.id}"
+    },
+    local.common_tags
+  )
 
-# }
+}
 
 
-# resource "aws_launch_template" "catalogue" {
+# resource "aws_launch_template" "main" {
 
-#   name                                 = "${local.common_name}-catalogue"
+#   name                                 = "${local.common_name}"
 #   image_id                             = aws_ami_from_instance.catalogue.id #AMI ID
 #   instance_initiated_shutdown_behavior = "terminate"
 #   instance_type                        = "t3.micro"
 #   update_default_version               = true
-#   vpc_security_group_ids               = [local.catalogue_sg_id]
+#   vpc_security_group_ids               = [local.sg_id]
 
 #   # Once instance are created these will become the instance tags
 #   tag_specifications {
@@ -92,7 +92,7 @@ resource "terraform_data" "main" {
 
 #     tags = merge(
 #       {
-#         Name = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
+#         Name = "${local.common_name}-${var.app_version}-${aws_instance.main.id}"
 #       },
 #       local.common_tags
 #     )
@@ -100,8 +100,8 @@ resource "terraform_data" "main" {
 #   # Launch template resource tags
 #   tags = merge(
 #     {
-#       Name = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
-#     },
+#       Name = "${local.common_name}-${var.app_version}-${aws_instance.catalogue.id}"
+#     },main
 #     local.common_tags
 #   )
 
@@ -147,13 +147,13 @@ resource "terraform_data" "main" {
 #   target_group_arns   = [aws_lb_target_group.catalogue.arn]
 
 
-#   # instance_refresh {
-#   #   strategy = "Rolling"
-#   #   preferences {
-#   #     min_healthy_percentage=50
-#   #   }
-#   #   triggers=["launch_template"]
-#   # }
+#    instance_refresh {
+#      strategy = "Rolling"
+#     preferences {
+#        min_healthy_percentage=50
+#      }
+#      triggers=["launch_template"]
+#    }
 
 #   dynamic "tag" {
 #     for_each = merge(
